7b95868c054f72a6965f7e8f2c4f17fcaf07081d
alejogs4 <alejogs4@gmail.com>
2017-10-29 04:02:02 -0500

    files

--- Nota musical/Form1.Designer.cs
+++ Nota musical/Form1.Designer.cs
@@ -29,28 +29,34 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.btn_inscribir = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_apellidos = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_nombre_estudiante = new System.Windows.Forms.TextBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txt_instructor = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txt_valor = new System.Windows.Forms.MaskedTextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txt_nombre_curso = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.txt_nombre_estudiante = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.txt_apellidos = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.btn_inscribir = new System.Windows.Forms.Button();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.txt_nombre_curso = new System.Windows.Forms.TextBox();
-            this.label7 = new System.Windows.Forms.Label();
-            this.txt_valor = new System.Windows.Forms.MaskedTextBox();
-            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.cmb_estudiantes = new System.Windows.Forms.ComboBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -70,8 +76,82 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Datos estudiante";
             // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(6, 169);
+            this.maskedTextBox1.Mask = "00/00/0000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(188, 20);
+            this.maskedTextBox1.TabIndex = 9;
+            this.maskedTextBox1.ValidatingType = typeof(System.DateTime);
+            // 
+            // btn_inscribir
+            // 
+            this.btn_inscribir.BackColor = System.Drawing.Color.Teal;
+            this.btn_inscribir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_inscribir.ForeColor = System.Drawing.Color.White;
+            this.btn_inscribir.Location = new System.Drawing.Point(10, 234);
+            this.btn_inscribir.Name = "btn_inscribir";
+            this.btn_inscribir.Size = new System.Drawing.Size(184, 45);
+            this.btn_inscribir.TabIndex = 8;
+            this.btn_inscribir.Text = "Inscribir estudiante";
+            this.btn_inscribir.UseVisualStyleBackColor = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(43, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(242, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Ingresa los datos para la inscripcion de estudiante";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Fecha de vencimiento";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Apellidos";
+            // 
+            // txt_apellidos
+            // 
+            this.txt_apellidos.Location = new System.Drawing.Point(6, 118);
+            this.txt_apellidos.Name = "txt_apellidos";
+            this.txt_apellidos.Size = new System.Drawing.Size(188, 20);
+            this.txt_apellidos.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nombre";
+            // 
+            // txt_nombre_estudiante
+            // 
+            this.txt_nombre_estudiante.Location = new System.Drawing.Point(6, 69);
+            this.txt_nombre_estudiante.Name = "txt_nombre_estudiante";
+            this.txt_nombre_estudiante.Size = new System.Drawing.Size(188, 20);
+            this.txt_nombre_estudiante.TabIndex = 0;
+            // 
             // groupBox2
             // 
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.txt_instructor);
+            this.groupBox2.Controls.Add(this.label8);
             this.groupBox2.Controls.Add(this.txt_valor);
             this.groupBox2.Controls.Add(this.label7);
             this.groupBox2.Controls.Add(this.txt_nombre_curso);
@@ -79,10 +159,81 @@
             this.groupBox2.Controls.Add(this.label5);
             this.groupBox2.Location = new System.Drawing.Point(423, 33);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(407, 353);
+            this.groupBox2.Size = new System.Drawing.Size(407, 313);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Datos cursos";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Teal;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(7, 234);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(188, 45);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Inscribir curso";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // txt_instructor
+            // 
+            this.txt_instructor.Location = new System.Drawing.Point(7, 169);
+            this.txt_instructor.Name = "txt_instructor";
+            this.txt_instructor.Size = new System.Drawing.Size(188, 20);
+            this.txt_instructor.TabIndex = 15;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(7, 152);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(97, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Instructor del curso";
+            // 
+            // txt_valor
+            // 
+            this.txt_valor.Location = new System.Drawing.Point(7, 117);
+            this.txt_valor.Mask = "9999999";
+            this.txt_valor.Name = "txt_valor";
+            this.txt_valor.Size = new System.Drawing.Size(188, 20);
+            this.txt_valor.TabIndex = 13;
+            this.txt_valor.ValidatingType = typeof(int);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(4, 100);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(77, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Valor del curso";
+            // 
+            // txt_nombre_curso
+            // 
+            this.txt_nombre_curso.Location = new System.Drawing.Point(7, 68);
+            this.txt_nombre_curso.Name = "txt_nombre_curso";
+            this.txt_nombre_curso.Size = new System.Drawing.Size(188, 20);
+            this.txt_nombre_curso.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(4, 50);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(90, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Nombre del curso";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(43, 26);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(133, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Ingresa los datos del curso";
             // 
             // tabControl1
             // 
@@ -108,127 +259,43 @@
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.cmb_estudiantes);
+            this.tabPage2.Controls.Add(this.label10);
+            this.tabPage2.Controls.Add(this.label9);
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage2.Size = new System.Drawing.Size(836, 443);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.Text = "Matricular estudiante";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
-            // txt_nombre_estudiante
+            // cmb_estudiantes
             // 
-            this.txt_nombre_estudiante.Location = new System.Drawing.Point(6, 69);
-            this.txt_nombre_estudiante.Name = "txt_nombre_estudiante";
-            this.txt_nombre_estudiante.Size = new System.Drawing.Size(188, 20);
-            this.txt_nombre_estudiante.TabIndex = 0;
+            this.cmb_estudiantes.FormattingEnabled = true;
+            this.cmb_estudiantes.Location = new System.Drawing.Point(225, 130);
+            this.cmb_estudiantes.Name = "cmb_estudiantes";
+            this.cmb_estudiantes.Size = new System.Drawing.Size(121, 21);
+            this.cmb_estudiantes.TabIndex = 2;
             // 
-            // label1
+            // label10
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(3, 50);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Nombre";
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(246, 105);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(57, 13);
+            this.label10.TabIndex = 1;
+            this.label10.Text = "Estudiante";
             // 
-            // label2
+            // label9
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(3, 102);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(49, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Apellidos";
-            // 
-            // txt_apellidos
-            // 
-            this.txt_apellidos.Location = new System.Drawing.Point(6, 118);
-            this.txt_apellidos.Name = "txt_apellidos";
-            this.txt_apellidos.Size = new System.Drawing.Size(188, 20);
-            this.txt_apellidos.TabIndex = 2;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(3, 152);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(112, 13);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "Fecha de vencimiento";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(43, 26);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(242, 13);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "Ingresa los datos para la inscripcion de estudiante";
-            // 
-            // btn_inscribir
-            // 
-            this.btn_inscribir.BackColor = System.Drawing.Color.Teal;
-            this.btn_inscribir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_inscribir.ForeColor = System.Drawing.Color.White;
-            this.btn_inscribir.Location = new System.Drawing.Point(10, 234);
-            this.btn_inscribir.Name = "btn_inscribir";
-            this.btn_inscribir.Size = new System.Drawing.Size(184, 45);
-            this.btn_inscribir.TabIndex = 8;
-            this.btn_inscribir.Text = "Inscribir estudiante";
-            this.btn_inscribir.UseVisualStyleBackColor = false;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(43, 26);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(133, 13);
-            this.label5.TabIndex = 9;
-            this.label5.Text = "Ingresa los datos del curso";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(4, 50);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(90, 13);
-            this.label6.TabIndex = 10;
-            this.label6.Text = "Nombre del curso";
-            // 
-            // txt_nombre_curso
-            // 
-            this.txt_nombre_curso.Location = new System.Drawing.Point(7, 68);
-            this.txt_nombre_curso.Name = "txt_nombre_curso";
-            this.txt_nombre_curso.Size = new System.Drawing.Size(188, 20);
-            this.txt_nombre_curso.TabIndex = 11;
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(4, 100);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(77, 13);
-            this.label7.TabIndex = 12;
-            this.label7.Text = "Valor del curso";
-            // 
-            // txt_valor
-            // 
-            this.txt_valor.Location = new System.Drawing.Point(7, 117);
-            this.txt_valor.Mask = "9999999";
-            this.txt_valor.Name = "txt_valor";
-            this.txt_valor.Size = new System.Drawing.Size(188, 20);
-            this.txt_valor.TabIndex = 13;
-            this.txt_valor.ValidatingType = typeof(int);
-            // 
-            // maskedTextBox1
-            // 
-            this.maskedTextBox1.Location = new System.Drawing.Point(6, 169);
-            this.maskedTextBox1.Mask = "00/00/0000";
-            this.maskedTextBox1.Name = "maskedTextBox1";
-            this.maskedTextBox1.Size = new System.Drawing.Size(188, 20);
-            this.maskedTextBox1.TabIndex = 9;
-            this.maskedTextBox1.ValidatingType = typeof(System.DateTime);
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(242, 15);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(340, 39);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "Matricular estudiante";
             // 
             // Form1
             // 
@@ -245,6 +312,8 @@
             this.groupBox2.PerformLayout();
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -258,7 +327,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txt_nombre_estudiante;
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.ComponentModel.BackgroundWorker backgroundWorker1;
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
@@ -270,6 +338,12 @@
         private System.Windows.Forms.TextBox txt_nombre_curso;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txt_instructor;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox cmb_estudiantes;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
     }
 }
 
